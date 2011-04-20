ebb33fa4c8efc63df1d9576dd545db698696e0b0
Mike McGrath <mmcgrath@redhat.com>
2011-04-20 15:49:25 -0500

    fixing openshift name

--- php/index.php
+++ php/index.php
@@ -1,6 +1,6 @@
-<html><title>Libra!</title>
+<html><title>OpenShift!</title>
 <body>
-<h1>Welcome to libra</h1>
+<h1>Welcome to OpenShift</h1>
 <p>Place your application here</p>
 <p>In order to commit to your new project, go to your projects git repo (created with the create_http command).  Make your changes, then run:</p>
 <pre>git commit -a -m 'Some commit message'
