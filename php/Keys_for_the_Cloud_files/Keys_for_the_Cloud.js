d0f46569a37f871f4743a8c587df6e704e99d5cd
LINICKX <linickx@gmail.com>
2011-05-31 21:07:38 +0100

    Welcome page with a bit of iWeb Style

---
+++ php/Keys_for_the_Cloud_files/Keys_for_the_Cloud.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20110531
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Keys_for_the_Cloud_files/Keys_for_the_CloudMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
