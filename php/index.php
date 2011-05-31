760ceaf0713d46f8182ab392ba52d7e67f2bb71c
LINICKX <linickx@gmail.com>
2011-05-31 13:20:53 +0100

    1st Commit!

--- php/index.php
+++ php/index.php
@@ -1,10 +1,5 @@
 <html><title>OpenShift!</title>
 <body>
-<h1>Welcome to OpenShift</h1>
-<p>Place your application here</p>
-<p>In order to commit to your new project, go to your projects git repo (created with the create_http command).  Make your changes, then run:</p>
-<pre>git commit -a -m 'Some commit message'
-git push</pre>
-<p>Then reload this page</p>
+<h1>Hello World</h1>
 </body>
 </html>
