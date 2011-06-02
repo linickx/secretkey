b1e9aa3ad87aefe616e26fac58e43f5123360b02
LINICKX <linickx@gmail.com>
2011-06-02 17:00:35 +0100

    Testing provider salt generation.

--- php/setup.php
+++ php/setup.php
@@ -2,14 +2,19 @@
 
 	/**
 	 
-		Set up Instructions
+		Set up Script.
 	 
 	 **/
 	
+	$n = mt_rand();
+	$randomstring = base_convert($n, 10, 36);
+	$ProviderSalt = hash('sha256', $randomstring);
+	
+	echo $ProviderSalt;
+	
+	#if (!$handle = fopen("./config.php", 'a')) {
+	#	echo "Cannot write file (config.php)";
+	#}
+	
+	
 	?>
-<html>
-<title>Set Up</title>
-<body>
-	Set up Instructions
-</body>
-</html>
