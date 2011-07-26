99929149b0c1bc21cbc5cc156e8ea771e18abe4f
Nick <linickx@gmail.com>
2011-07-26 20:44:25 +0100

    OPENSHIFT: Troubleshooting SSL 1

--- php/index.php
+++ php/index.php
@@ -9,7 +9,10 @@
 	if($_SERVER['HTTPS']!=="on") { // Require SSL, we need to really!
 	
 		$redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-		header("Location:$redirect");
+		#header("Location:$redirect");
+		
+		print_r($_SERVER);
+		exit;
 	}
 	
 	if (!file_exists("../../data/config.php")) { 
