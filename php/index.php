065f9097d021170fad8f3b12950fed8b1b9f277b
Nick <linickx@gmail.com>
2011-07-26 20:48:37 +0100

    OPENSHIFT: Remove SSL detection due to reverse-proxy issue, SSL works but is not detected by $_SERVER as it appears the SSL termination is being perfomed off box.

--- php/index.php
+++ php/index.php
@@ -6,14 +6,6 @@
 	 
 	 **/
 	
-	if($_SERVER['HTTPS']!=="on") { // Require SSL, we need to really!
-	
-		$redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-		#header("Location:$redirect");
-		
-		print_r($_SERVER);
-		exit;
-	}
 	
 	if (!file_exists("../../data/config.php")) { 
 		
