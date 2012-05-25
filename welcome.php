671972d9cfe4cc0b4b5e866c744c59d5093b56d5
Nick <linickx@gmail.com>
2012-05-25 14:20:05 +0100

    remove <?xml for php 5.3 compatability

--- welcome.php
+++ welcome.php
@@ -7,7 +7,6 @@
 	 **/
 	
 	?>
-<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 
@@ -188,5 +187,3 @@ new NavBar('widget0', 'Scripts/Widgets/Navbar', 'Scripts/Widgets/SharedResources
 </div>
 </body>
 </html>
-
-
